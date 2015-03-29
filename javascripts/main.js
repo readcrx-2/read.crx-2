eaeeae0954552cad43f31b5267937062e92fd02f
Kazuki Hashimoto <eru.tndl@gmail.com>
2015-03-29 15:31:12 +0900

    Update: ダウンロードリンクの表示を自動化

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,11 @@
-console.log('This would be the main JS file.');
+$(function() {
+  // 最新バージョン取得
+  $.get('http://eru.github.io/read.crx-2/updates.xml', function(data) {
+    if (data) {
+      var download = $('updatecheck', data).attr('codebase');
+      var filename = download.substring(download.lastIndexOf('/') + 1, download.length);
+      $('#download').attr('href', download);
+      $('#download-filename').text(filename);
+    }
+  }, 'xml');
+});

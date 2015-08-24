443937513865c4f29f1afa7ea61eaa64d46c7a00
Kazuki Hashimoto <eru.tndl@gmail.com>
2015-08-24 09:59:14 +0900

    Fix: crxファイルのバージョン取得処理のプロトコルを削除

--- javascripts/beta.js
+++ javascripts/beta.js
@@ -1,6 +1,6 @@
 $(function() {
   // 最新バージョン取得
-  $.get('http://readcrx-2.github.io/read.crx-2/updates.xml', function(data) {
+  $.get('//readcrx-2.github.io/read.crx-2/updates.xml', function(data) {
     if (data) {
       var download = $('app[appid="mglmonflpdcckjdpjnpenfdmkbmggmgm"] > updatecheck', data).attr('codebase');
       var filename = download.substring(download.lastIndexOf('/') + 1, download.length);

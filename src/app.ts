8b1078ddd9fc2aee299e9519f2c7fe87b3abceeb
S <S@mail.minecraft.jp.net>
2017-05-13 16:15:30 +0900

    Update: app.configから合計最大同時接続数を変更できるように refs #299

--- src/app.ts
+++ src/app.ts
@@ -288,7 +288,8 @@ namespace app {
       ["image_replace_dat_obj", "[]"],
       ["image_replace_dat", "^https?:\\/\\/(?:www\\.youtube\\.com\\/watch\\?(?:.+&)?v=|youtu\\.be\\/)([\\w\\-]+).*\thttps://img.youtube.com/vi/$1/default.jpg\nhttp:\\/\\/(?:www\\.)?nicovideon?\\.jp\\/(?:(?:watch|thumb)(?:_naisho)?(?:\\?v=|\\/)|\\?p=)(?!am|fz)[a-z]{2}(\\d+)\thttp://tn-skr.smilevideo.jp/smile?i=$1\n\\.(png|jpe?g|gif|bmp|webp)([\\?#:].*)?$\t.$1$2"],
       ["replace_str_txt_obj", "[]"],
-      ["replace_str_txt", ""]
+      ["replace_str_txt", ""],
+      ["max_connection", "4"]
     ]);
 
     private _cache = new Map<string, string>();

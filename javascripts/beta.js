dbe473ddf5f7596385333685950f112f686a618c
S <s@4na.xyz>
2018-09-09 19:00:39 +0900

    Update: jQuery 2.1.3 -> 3.3.1

--- javascripts/beta.js
+++ javascripts/beta.js
@@ -1,11 +1,14 @@
 $(function() {
   // 最新バージョン取得
-  $.get('//readcrx-2.github.io/read.crx-2/updates.xml', function(data) {
+  $.get({
+    url: '//readcrx-2.github.io/read.crx-2/updates.xml',
+    dataType: 'xml',
+  }).done(function(data) {
     if (data) {
       var download = $('app[appid="mglmonflpdcckjdpjnpenfdmkbmggmgm"] > updatecheck', data).attr('codebase');
       var filename = download.substring(download.lastIndexOf('/') + 1, download.length);
       $('#download').attr('href', download);
       $('#download-filename').text(filename);
     }
-  }, 'xml');
+  });
 });
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,20 +1,27 @@
 $(function() {
   // 最新バージョン取得(crx)
-  $.get('//readcrx-2.github.io/read.crx-2/updates.xml', function(data) {
+  $.get({
+    url: '//readcrx-2.github.io/read.crx-2/updates.xml',
+    dataType: 'xml',
+  }).done(function(data) {
     if (data) {
       var download = $('app[appid="eaibgccboimjelecbmgfjhakekfdcpeh"] > updatecheck', data).attr('codebase');
       var filename = download.substring(download.lastIndexOf('/') + 1, download.length);
       $('#downloadcrx').attr('href', download);
       $('#downloadcrx-filename').text(filename);
     }
-  }, 'xml');
+  });
+
   // 最新バージョン取得(xpi)
-  $.get('//readcrx-2.github.io/read.crx-2/updates_firefox.json', function(data) {
+  $.get({
+    url: '//readcrx-2.github.io/read.crx-2/updates_firefox.json',
+    dataType: 'json',
+  }).done(function(data) {
     if (data) {
       var download = data.addons["read.crx2@read.crx"].updates[0].update_link;
       var filename = download.substring(download.lastIndexOf('/') + 1, download.length);
       $('#downloadxpi').attr('href', download);
       $('#downloadxpi-filename').text(filename);
     }
-  }, 'json');
+  });
 });

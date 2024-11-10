d6b9898642bce798f5867ba3f6ec40465d9e28ea
Kazuki Hashimoto <eru.tndl@gmail.com>
2024-11-10 17:37:43 +0900

    FIX: read.cgi ver 07.6.2に暫定対応

--- src/core/Thread.js
+++ src/core/Thread.js
@@ -510,6 +510,11 @@ URLが間違っているか過去ログに移動せずに削除されていま�
       reg =
         /<div class="post"[^<>]*><div class="meta"><span class="number">\d+<\/span><span class="name"><b>(?:<a href="mailto:([^<>]*)">|<font [^<>]*>)?(.*?)(?:<\/(?:a|font)>)?<\/b><\/span><span class="date">(.*)<\/span><\/div><div class="message">(?:<span class="escaped">)? ?(.*)(?:<\/span>)/;
       separator = "</div></div><br>";
+    } else if (text.match(/<footer[^<>]*><br>read\.cgi ver 07\.6/)) {
+      titleReg = /<(?:div|h1) id="threadtitle">(.*)\n?<\/(?:div|h1)>/;
+      reg =
+        /<span class="postid">\d+<\/span><span class="postusername"><b>(?:<a href="mailto:([^<>]*)">|<font [^<>]*>)?(.*?)(?:<\/(?:a|font)>)?<\/b><\/span>(?:<span style=".*">.*<\/span>)?<\/div>(?:<span style=".*">)?<span class="date">(.*)<\/span><\/div><div class="post-content"> ?(.*)/;
+      separator = "</div></div>";
     } else if (text.match(/<footer[^<>]*><br>read\.cgi ver 0(7|8)/)) {
       titleReg = /<(?:div|h1) id="threadtitle">(.*)\n?<\/(?:div|h1)>/;
       reg =
